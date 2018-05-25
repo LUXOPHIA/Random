5dc95f459083ab90b5f8150d0596da397cb7d81b
luxidea <contact@luxidea.net>
2018-05-26 00:48:32 +0900

    乱数を可視化。

--- Main.pas
+++ Main.pas
@@ -7,7 +7,9 @@ uses
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
   FMX.StdCtrls, FMX.Objects, FMX.Controls.Presentation,
   LUX, LUX.D3, LUX.D4,
-  LUX.Random, LUX.Random.Xorshift;
+  LUX.Random,
+  LUX.Random.LCG,
+  LUX.Random.Xorshift;
 
 type
   TForm1 = class(TForm)
@@ -16,21 +18,27 @@ type
     Image1: TImage;
     Panel1: TPanel;
       GroupBox1: TGroupBox;
-        RadioButton1: TRadioButton;
-        RadioButton2: TRadioButton;
-        RadioButton3: TRadioButton;
-        RadioButton4: TRadioButton;
-      Button1: TButton;
+        RadioButtonAL1: TRadioButton;
+        RadioButtonAL2: TRadioButton;
+        RadioButtonAL3: TRadioButton;
+        RadioButtonAX1: TRadioButton;
+        RadioButtonAX2: TRadioButton;
+        RadioButtonAX3: TRadioButton;
+        RadioButtonAX4: TRadioButton;
+    Timer1: TTimer;
     procedure FormCreate(Sender: TObject);
-    procedure RadioButton1Change(Sender: TObject);
-    procedure RadioButton2Change(Sender: TObject);
-    procedure RadioButton3Change(Sender: TObject);
-    procedure RadioButton4Change(Sender: TObject);
-    procedure Button1Click(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+    procedure RadioButtonAL1Change(Sender: TObject);
+    procedure RadioButtonAL2Change(Sender: TObject);
+    procedure RadioButtonAL3Change(Sender: TObject);
+    procedure RadioButtonAX1Change(Sender: TObject);
+    procedure RadioButtonAX2Change(Sender: TObject);
+    procedure RadioButtonAX3Change(Sender: TObject);
+    procedure RadioButtonAX4Change(Sender: TObject);
   private
     { private 宣言 }
     ///// メソッド
-    procedure ShowRands;
+    procedure ShowRandoms;
   public
     { public 宣言 }
     _RandomC  :CRandom;
@@ -52,10 +60,9 @@ uses System.Math, System.Threading;
 
 /////////////////////////////////////////////////////////////////////// メソッド
 
-procedure TForm1.ShowRands;
+procedure TForm1.ShowRandoms;
 var
    B :TBitmapData;
-   C :Single;
 begin
      with Image1.Bitmap do
      begin
@@ -63,20 +70,20 @@ begin
 
           Map( TMapAccess.Write, B );
 
-          TParallel.For( 0, _ThreadsN-1, procedure( Y:Integer )
+          TParallel.For( 16, 0, _ThreadsN-1, procedure( Y:Integer )
           var
              X :Integer;
              P :PAlphaColor;
+             C :Single;
           begin
-               P := B.GetScanline( Y );
+               P := B.GetScanline( _ThreadsN-1 - Y );
 
                for X := 0 to _SequensN-1 do
                begin
                     C := Power( _Randoms[ Y, X ], 1 / 2.2 );
 
-                    P^ := $FF000000 or $00010101 * Floor( 256 * C );  Inc( P );
+                    P^ := TAlphaColorF.Create( C, C, C ).ToAlphaColor;  Inc( P );
                end;
-
           end );
 
           Unmap( B );
@@ -85,58 +92,70 @@ end;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
 
-/////////////////////////////////////////////////////////////////////// メソッド
-
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
 procedure TForm1.FormCreate(Sender: TObject);
-begin
-     RadioButton1.IsChecked := True;
-end;
-
-////////////////////////////////////////////////////////////////////////////////
-
-procedure TForm1.RadioButton1Change(Sender: TObject);
-begin
-     _RandomC := TRandomXOR32;
-end;
-
-procedure TForm1.RadioButton2Change(Sender: TObject);
-begin
-     _RandomC := TRandomXOR64;
-end;
-
-procedure TForm1.RadioButton3Change(Sender: TObject);
-begin
-     _RandomC := TRandomXOR96;
-end;
-
-procedure TForm1.RadioButton4Change(Sender: TObject);
-begin
-     _RandomC := TRandomXOR128;
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TForm1.Button1Click(Sender: TObject);
 begin
      _ThreadsN := 256;
      _SequensN := 256;
 
      SetLength( _Randoms, _ThreadsN, _SequensN );
 
-     TParallel.For( 0, _ThreadsN-1, procedure( Y:Integer )
-     var
-        R :IRandom;
-        X :Integer;
+     RadioButtonAL1.IsChecked := True;
+end;
+
+////////////////////////////////////////////////////////////////////////////////
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+var
+   Y, X :Integer;
+   R :IRandom;
+begin
+     for Y := 0 to _ThreadsN-1 do
      begin
           R := _RandomC.Create;
 
           for X := 0 to _SequensN-1 do _Randoms[ Y, X ] := R.Value;
+     end;
 
-     end );
+     ShowRandoms;
+end;
 
-     ShowRands;
+//------------------------------------------------------------------------------
+
+procedure TForm1.RadioButtonAL1Change(Sender: TObject);
+begin
+     _RandomC := TRandomLCG32;
+end;
+
+procedure TForm1.RadioButtonAL2Change(Sender: TObject);
+begin
+     _RandomC := TRandomLCG48;
+end;
+
+procedure TForm1.RadioButtonAL3Change(Sender: TObject);
+begin
+     _RandomC := TRandomLCG64;
+end;
+
+procedure TForm1.RadioButtonAX1Change(Sender: TObject);
+begin
+     _RandomC := TRandomXOR32;
+end;
+
+procedure TForm1.RadioButtonAX2Change(Sender: TObject);
+begin
+     _RandomC := TRandomXOR64;
+end;
+
+procedure TForm1.RadioButtonAX3Change(Sender: TObject);
+begin
+     _RandomC := TRandomXOR96;
+end;
+
+procedure TForm1.RadioButtonAX4Change(Sender: TObject);
+begin
+     _RandomC := TRandomXOR128;
 end;
 
 end. //######################################################################### ■
