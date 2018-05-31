84d4f98bc2a6eb61129a16b5098340cf8382e904
luxidea <contact@luxidea.net>
2018-05-31 21:06:39 +0900

    TRandom32XOS128x64, TRandom64XOS128x64, TRandom64XOS256x128 クラスを追加。

--- _LIBRARY/LUXOPHIA/LUX/Random/LUX.Random.Xoshiro.pas
+++ _LIBRARY/LUXOPHIA/LUX/Random/LUX.Random.Xoshiro.pas
@@ -17,13 +17,22 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
          TRandom32XOS128            = class;
            TRandom32XOS128p         = class;
            TRandom32XOS128ss        = class;
+           TRandom32XOS128x64       = class;
+             TRandom32XOS128x64p    = class;
+             TRandom32XOS128x64ss   = class;
        TRandom64XOS<_TSeed_:record> = class;
          TRandom64XOS128            = class;
            TRandom64XOS128p         = class;
            TRandom64XOS128ss        = class;
+           TRandom64XOS128x64       = class;
+             TRandom64XOS128x64p    = class;
+             TRandom64XOS128x64ss   = class;
          TRandom64XOS256            = class;
            TRandom64XOS256p         = class;
            TRandom64XOS256ss        = class;
+           TRandom64XOS256x128      = class;
+             TRandom64XOS256x128p   = class;
+             TRandom64XOS256x128ss  = class;
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
 
@@ -103,7 +112,6 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
      protected
        ///// メソッド
        procedure CalcNextSeed; override;
-       procedure CalcNextSeed64;
      public
        constructor CreateFromRand( const Random_:IRandom ); overload; override;
      end;
@@ -128,6 +136,36 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
      public
      end;
 
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom32XOS128x64
+
+     TRandom32XOS128x64 = class( TRandom32XOS128 )
+     private
+     protected
+       ///// メソッド
+       procedure CalcNextSeed; override;
+     public
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom32XOS128x64p
+
+     TRandom32XOS128x64p = class( TRandom32XOS128x64 )
+     private
+     protected
+       ///// メソッド
+       function CalcRandInt32u :Int32u; override;
+     public
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom32XOS128x64ss
+
+     TRandom32XOS128x64ss = class( TRandom32XOS128x64 )
+     private
+     protected
+       ///// メソッド
+       function CalcRandInt32u :Int32u; override;
+     public
+     end;
+
      //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom64XOS128
 
      TRandom64XOS128 = class( TRandom64XOS<TInt64u2D> )
@@ -135,7 +173,6 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
      protected
        ///// メソッド
        procedure CalcNextSeed; override;
-       procedure CalcNextSeed64;
      public
        constructor CreateFromRand( const Random_:IRandom ); overload; override;
      end;
@@ -160,6 +197,36 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
      public
      end;
 
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom64XOS128x64
+
+     TRandom64XOS128x64 = class( TRandom64XOS128 )
+     private
+     protected
+       ///// メソッド
+       procedure CalcNextSeed; override;
+     public
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom64XOS128x64p
+
+     TRandom64XOS128x64p = class( TRandom64XOS128x64 )
+     private
+     protected
+       ///// メソッド
+       function CalcRandInt64u :Int64u; override;
+     public
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom64XOS128x64ss
+
+     TRandom64XOS128x64ss = class( TRandom64XOS128x64 )
+     private
+     protected
+       ///// メソッド
+       function CalcRandInt64u :Int64u; override;
+     public
+     end;
+
      //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom64XOS256
 
      TRandom64XOS256 = class( TRandom64XOS<TInt64u4D> )
@@ -167,7 +234,6 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
      protected
        ///// メソッド
        procedure CalcNextSeed; override;
-       procedure CalcNextSeed128;
      public
        constructor CreateFromRand( const Random_:IRandom ); overload; override;
      end;
@@ -185,6 +251,36 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
      //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom64XOS256ss
 
      TRandom64XOS256ss = class( TRandom64XOS256 )
+     private
+     protected
+       ///// メソッド
+       function CalcRandInt64u :Int64u; override;
+     public
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom64XOS256x128
+
+     TRandom64XOS256x128 = class( TRandom64XOS256 )
+     private
+     protected
+       ///// メソッド
+       procedure CalcNextSeed; override;
+     public
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom64XOS256x128p
+
+     TRandom64XOS256x128p = class( TRandom64XOS256x128 )
+     private
+     protected
+       ///// メソッド
+       function CalcRandInt64u :Int64u; override;
+     public
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom64XOS256x128ss
+
+     TRandom64XOS256x128ss = class( TRandom64XOS256x128 )
      private
      protected
        ///// メソッド
@@ -336,42 +432,6 @@ begin
      end;
 end;
 
-//------------------------------------------------------------------------------
-
-procedure TRandom32XOS128.CalcNextSeed64;
-const
-     JUMP :array [ 0..4-1 ] of Int32u = ( $8764000b,
-                                          $f542d2d3,
-                                          $6fa035c3,
-                                          $77f2db5b );
-var
-   S :TInt32u4D;
-   I, B :Int32s;
-begin
-     S := TInt32u4D.Create( 0, 0, 0, 0 );
-
-     for I := 0 to 4-1 do
-     begin
-          for B := 0 to 32 - 1 do
-          begin
-               if JUMP[ I ] and ( 1 shl B ) = 1 then
-               begin
-                    with S do
-                    begin
-                         X := X xor _Seed.X;
-                         Y := Y xor _Seed.Y;
-                         Z := Z xor _Seed.Z;
-                         W := W xor _Seed.W;
-                    end;
-               end;
-
-               CalcNextSeed;
-          end;
-     end;
-
-     _Seed := S;
-end;
-
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
 
 constructor TRandom32XOS128.CreateFromRand( const Random_:IRandom );
@@ -416,6 +476,84 @@ end;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
 
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom32XOS128x64
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TRandom32XOS128x64.CalcNextSeed;
+const
+     JUMP :array [ 0..4-1 ] of Int32u = ( $8764000b,
+                                          $f542d2d3,
+                                          $6fa035c3,
+                                          $77f2db5b );
+var
+   S :TInt32u4D;
+   I, B :Int32s;
+begin
+     S := TInt32u4D.Create( 0, 0, 0, 0 );
+
+     for I := 0 to 4-1 do
+     begin
+          for B := 0 to 32 - 1 do
+          begin
+               if JUMP[ I ] and ( 1 shl B ) = 1 then
+               begin
+                    with S do
+                    begin
+                         X := X xor _Seed.X;
+                         Y := Y xor _Seed.Y;
+                         Z := Z xor _Seed.Z;
+                         W := W xor _Seed.W;
+                    end;
+               end;
+
+               inherited CalcNextSeed;
+          end;
+     end;
+
+     _Seed := S;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom32XOS128x64p
+
+{ http://xoshiro.di.unimi.it/xoshiro128plus.c }
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+function TRandom32XOS128x64p.CalcRandInt32u :UInt32;
+begin
+     with _Seed do Result := X + W;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom32XOS128x64ss
+
+{ http://xoshiro.di.unimi.it/xoshiro128plus.c }
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+function TRandom32XOS128x64ss.CalcRandInt32u :UInt32;
+begin
+     Result := Rot( _Seed.X * 5, 7 ) * 9;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom64XOS128
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
@@ -437,38 +575,6 @@ begin
           _Seed.X := Rot( X, 24 ) xor Y xor ( Y shl 16 );  // a, b
           _Seed.Y := Rot( Y, 37 );                         // c
      end;
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TRandom64XOS128.CalcNextSeed64;
-const
-     JUMP :array [ 0..2-1 ] of Int64u = ( $df900294d8f554a5,
-                                          $170865df4b3201fc );
-var
-   S :TInt64u2D;
-   I, B :Int64s;
-begin
-     S := TInt64u2D.Create( 0, 0 );
-
-     for I := 0 to 2-1 do
-     begin
-          for B := 0 to 64 - 1 do
-          begin
-               if JUMP[ I ] and ( 1 shl B ) = 1 then
-               begin
-                    with S do
-                    begin
-                         X := X xor _Seed.X;
-                         Y := Y xor _Seed.Y;
-                    end;
-               end;
-
-               CalcNextSeed;
-          end;
-     end;
-
-     _Seed := S;
 end;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
@@ -513,6 +619,80 @@ end;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
 
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom64XOS128x64
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TRandom64XOS128x64.CalcNextSeed;
+const
+     JUMP :array [ 0..2-1 ] of Int64u = ( $df900294d8f554a5,
+                                          $170865df4b3201fc );
+var
+   S :TInt64u2D;
+   I, B :Int64s;
+begin
+     S := TInt64u2D.Create( 0, 0 );
+
+     for I := 0 to 2-1 do
+     begin
+          for B := 0 to 64 - 1 do
+          begin
+               if JUMP[ I ] and ( 1 shl B ) = 1 then
+               begin
+                    with S do
+                    begin
+                         X := X xor _Seed.X;
+                         Y := Y xor _Seed.Y;
+                    end;
+               end;
+
+               inherited CalcNextSeed;
+          end;
+     end;
+
+     _Seed := S;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom64XOS128x64p
+
+{ http://xoshiro.di.unimi.it/xoroshiro128plus.c }
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+function TRandom64XOS128x64p.CalcRandInt64u :Int64u;
+begin
+	   with _Seed do Result := X + Y;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom64XOS128x64ss
+
+{ http://xoshiro.di.unimi.it/xoroshiro128starstar.c }
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+function TRandom64XOS128x64ss.CalcRandInt64u :Int64u;
+begin
+	   Result := Rot( _Seed.X * 5, 7 ) * 9;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom64XOS256
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
@@ -538,42 +718,6 @@ begin
 
           W := Rot( W, 45 );
      end;
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TRandom64XOS256.CalcNextSeed128;
-const
-     JUMP :array [ 0..4-1 ] of Int64u = ( $180ec6d33cfd0aba,
-                                          $d5a61266f0c9392c,
-                                          $a9582618e03fc9aa,
-                                          $39abdc4529b1661c );
-var
-   S :TInt64u4D;
-   I, B :Int64u;
-begin
-     S := TInt64u4D.Create( 0, 0, 0, 0 );
-
-     for I := 0 to 4-1 do
-     begin
-          for B := 0 to 64-1 do
-          begin
-               if JUMP[ I ] and ( 1 shl B ) = 0 then
-               begin
-                    with S do
-                    begin
-                         X := X xor _Seed.X;
-                         Y := Y xor _Seed.Y;
-                         Z := Z xor _Seed.Z;
-                         W := W xor _Seed.W;
-                    end;
-               end;
-
-               CalcNextSeed;
-          end;
-     end;
-
-     _Seed := S;
 end;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
@@ -614,6 +758,84 @@ end;
 /////////////////////////////////////////////////////////////////////// メソッド
 
 function TRandom64XOS256ss.CalcRandInt64u :Int64u;
+begin
+     Result := Rot( _Seed.Y * 5, 7 ) * 9;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom64XOS256x128
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TRandom64XOS256x128.CalcNextSeed;
+const
+     JUMP :array [ 0..4-1 ] of Int64u = ( $180ec6d33cfd0aba,
+                                          $d5a61266f0c9392c,
+                                          $a9582618e03fc9aa,
+                                          $39abdc4529b1661c );
+var
+   S :TInt64u4D;
+   I, B :Int64u;
+begin
+     S := TInt64u4D.Create( 0, 0, 0, 0 );
+
+     for I := 0 to 4-1 do
+     begin
+          for B := 0 to 64-1 do
+          begin
+               if JUMP[ I ] and ( 1 shl B ) = 0 then
+               begin
+                    with S do
+                    begin
+                         X := X xor _Seed.X;
+                         Y := Y xor _Seed.Y;
+                         Z := Z xor _Seed.Z;
+                         W := W xor _Seed.W;
+                    end;
+               end;
+
+               inherited CalcNextSeed;
+          end;
+     end;
+
+     _Seed := S;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom64XOS256x128p
+
+{ http://xoshiro.di.unimi.it/xoshiro256plus.c }
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+function TRandom64XOS256x128p.CalcRandInt64u :Int64u;
+begin
+     with _Seed do Result := X + W;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRandom64XOS256x128ss
+
+{ http://xoshiro.di.unimi.it/xoshiro256starstar.c }
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+function TRandom64XOS256x128ss.CalcRandInt64u :Int64u;
 begin
      Result := Rot( _Seed.Y * 5, 7 ) * 9;
 end;
